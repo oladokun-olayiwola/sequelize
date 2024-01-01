6056b37162c7fe37ade40f13950bf6c6decce042
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-01 23:53:23 +0100

    Validate user middleware

--- JWT/middlewares.ts
+++ JWT/middlewares.ts
@@ -2,6 +2,16 @@ import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 import { UserData } from "./IUser";
 
+export const validateUserType = (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password, status }: UserData = req.body;
+  
+    if (name && email && password && status) {
+      next();
+    } else {
+      res.status(400).json({error: true, message: "Invalid Request Body"});
+    }
+  };
+
 export const errorHandler = (
     err: Error,
     req: Request,
